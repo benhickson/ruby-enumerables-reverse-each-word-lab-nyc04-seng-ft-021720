09e91dd4d2abeb0a8a6a6dbab0215aaea5442208
Ben Hickson <bzhickson@gmail.com>
2020-02-12 01:47:41 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -11,3 +11,15 @@
   
 #   output
 # end
+
+def reverse_each_word(sentence)
+  
+  output = []
+  sentence.split.each do |word|
+    output.push(word.reverse)
+  end
+  
+  output = output.join(' ')
+  
+  output
+end
